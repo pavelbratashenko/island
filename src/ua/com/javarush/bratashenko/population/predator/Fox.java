4fd929709a7f92a6c5a058194ad6c02d06f602f0
bratashenko.p <bratashenko.p@gmail.com>
2022-08-17 21:28:04 +0300

    creating a hierarchy.

---
+++ src/ua/com/javarush/bratashenko/population/predator/Fox.java
@@ -0,0 +1,25 @@
+package ua.com.javarush.bratashenko.population.predator;
+
+import ua.com.javarush.bratashenko.population.Predator;
+
+public class Fox extends Predator {
+    @Override
+    public void movement() {
+
+    }
+
+    @Override
+    public void reproduction() {
+
+    }
+
+    @Override
+    public void eat() {
+
+    }
+
+    @Override
+    public void dieOfHunger() {
+
+    }
+}
